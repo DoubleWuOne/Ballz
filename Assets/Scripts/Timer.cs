9443f98bbfd850c0d36a387c2f098c8f78b6482e
Jakub Pietraszkiewicz <kubronis@gmail.com>
2021-06-29 23:08:06 +0200

    Update Timer.cs

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI text;
-    // Start is called before the first frame update
+    private float startTime;
     void Start()
     {
-        
+        startTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        text.text = minutes + ":" + seconds;
     }
 }
